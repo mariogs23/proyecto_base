77cdff2fff85fd276dc50610c435a2d16f8956d4
mariogs23 <mario.garcia17@alu.uclm.es>
2016-10-13 17:45:38 +0200

    clase 13/10/2016

--- cliente/js/control-juego.js
+++ cliente/js/control-juego.js
@@ -1,6 +1,6 @@
 
-//var url="https://pasaniveles.herokuapp.com/";
-var url="http://127.0.0.1:1338/";
+var url="https://pasaniveles.herokuapp.com/";
+//var url="http://127.0.0.1:1338/";
 
 //Funciones que modifican el index
 
